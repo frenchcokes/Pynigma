6d36010201ddd0ad553953ee69019187d07888a6
frenchcokes <144563579+frenchcokes@users.noreply.github.com>
2023-12-29 20:56:54 -0700

    modified rotor structuring

--- pynigma.py
+++ pynigma.py
@@ -1,35 +1,72 @@
 class EnigmaMachine():
-    def __init__(self, rotors, reflector):
+    def __init__(self, rotors, reflector, isShowSteps):
         self.rotors = rotors
         self.reflector = reflector
-
-    def encrypt(self, letter):
-        encryptValue = letter
-        isNextRotorRotate = True
+        self.isShowSteps = isShowSteps
+    
+    def checkRotate(self):
+        rotateNext = True
 
         for rotor in self.rotors:
-            if (isNextRotorRotate == True):
+            if(rotateNext == True):
+                rotateNext = rotor.isNextRotorRotate()
                 rotor.rotate()
-            isNextRotorRotate = rotor.isNextRotorRotate()
+            else:
+                break
+    
+    def getRotorValues(self):
+        rotorValues = []
+        for rotor in self.rotors:
+            rotorValues.append(rotor.getCurrentPostion())
+        rotorValues.reverse()
+        return rotorValues
+
+    def encrypt(self, letter):
+        self.checkRotate()
+
+        encryptValue = letter
+
+        if(self.isShowSteps == True):
+            print("State: " + str(self.getRotorValues()))
+            print("Input: " + letter)
+        for rotor in self.rotors:
             encryptValue = rotor.encrypt(encryptValue)
-
+            if(self.isShowSteps == True):
+                print(rotor.name + " encryption: " + encryptValue)
+        
         encryptValue = self.reflector.encrypt(encryptValue)
+        if(self.isShowSteps == True):
+                print(self.reflector.name + " encryption: " + encryptValue)
 
-        reversedOrderRotors = self.rotors.reverse()
+        rotorsCopy = self.rotors.copy()
+        rotorsCopy.reverse()
+        reversedOrderRotors = rotorsCopy
 
         for rotor in reversedOrderRotors:
-            if (isNextRotorRotate == True):
-                rotor.rotate()
-            isNextRotorRotate = rotor.isNextRotorRotate()
             encryptValue = rotor.encrypt(encryptValue)
+            if(self.isShowSteps == True):
+                print(rotor.name + " encryption: " + encryptValue)
 
         return encryptValue
     
+    def setRotorCurrentPositions(self, currentPositions):
+        assert len(currentPositions) == len(self.rotors), "Number of positions not equal to number of rotors."
+        for x in range(len(self.rotors)):
+            self.rotors[x].setCurrentPosition(currentPositions[x])
+
+    def setRotorRotatePositions(self, rotatePositions):
+        assert len(rotatePositions) == len(self.rotors), "Number of positions not equal to number of rotors."
+        for x in range(len(self.rotors)):
+            self.rotors[x].setRotatePosition(rotatePositions[x])
+
     def addRotors(self, rotors):
         self.rotors = rotors
 
     def addReflector(self, reflector):
         self.reflector = reflector
+
+    def setIsShowSteps(self, bool):
+        self.isShowSteps(bool)
 
 class Reflector():
     def __init__(self, name, wiring):
@@ -37,7 +74,7 @@ class Reflector():
         self.wiring = wiring
     
     def encrypt(self, letter):
-        position = ord(letter) - 97
+        position = ord(letter) - 65
         encryptedLetter = self.wiring[position]
         return encryptedLetter
 
@@ -55,22 +92,35 @@ class Rotor():
 
     def rotate(self):
         nextPosition = self.currentPosition + 1
-        if(nextPosition == len(self.wiring) ):
+        if(nextPosition > 25 ):
             self.currentPosition = 0
         else:
             self.currentPosition = nextPosition
 
-    #Called after rotate()
     def isNextRotorRotate(self):
-        if(self.rotatePosition == (self.currentPosition - 1) ):
-            return True
-        elif(self.rotatePosition == 25 and self.currentPosition == 0):
+        if (self.currentPosition == self.rotatePosition):
             return True
         else:
             return False
 
-
     def encrypt(self, letter):
-        position = ord(letter) - 97 + self.currentPosition
-        encryptedLetter = self.wiring[position]
+        inputPosition = self.currentPosition #0-25
+        letterPosition = ord(letter) - 65 #0-25
+        convertedPosition = letterPosition - inputPosition
+
+        if(convertedPosition > 25):
+            convertedPosition = convertedPosition - 26
+        elif(convertedPosition < 0):
+            convertedPosition = convertedPosition + 26
+
+        encryptedLetter = self.wiring[convertedPosition]
         return encryptedLetter
+    
+    def setCurrentPosition(self, newCurrentPosition):
+        self.currentPosition = newCurrentPosition
+
+    def setRotatePosition(self, newRotatePosition):
+        self.rotatePosition = newRotatePosition
+
+    def getCurrentPostion(self):
+        return self.currentPosition
--- tester.py
+++ tester.py
@@ -1,8 +1,25 @@
 import pynigma
 
 if __name__ == "__main__":
-    testRotor = pynigma.Rotor("Basic", "bcdefghijklmnopqrstuvwxyza")
-    
-    print(testRotor.encrypt("a"))
-    testRotor.rotate()
-    print(testRotor.encrypt("a"))
+    rotorI = pynigma.Rotor("I", "EKMFLGDQVZNTOWYHXUSPAIBRCJ", 0, 0)
+    rotorII = pynigma.Rotor("II", "AJDKSIRUXBLHWTMCQGZNPYFVOE", 0, 0)
+    rotorIII = pynigma.Rotor("III", "BDFHJLCPRTXVZNYEIWGAKMUSQO", 0, 0)
+
+    #rotorI = pynigma.Rotor("I", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 0)
+    #rotorII = pynigma.Rotor("II", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 0)
+    #rotorIII = pynigma.Rotor("III", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 0)
+    rotorIV = pynigma.Rotor("IV", "ESOVPZJAYQUIRHXLNFTGKDCMWB", 0, 0)
+    rotorV = pynigma.Rotor("V", "VZBRGITYUPSDNHLXAWMJQOFECK", 0, 0)
+
+    reflectorB = pynigma.Reflector("UKW-B","YRUHQSLDPXNGOKMIEBFZCWVJAT")
+
+    enigmaMachine = pynigma.EnigmaMachine([rotorI, rotorII, rotorIII], reflectorB, True)
+
+    enigmaMachine.setRotorCurrentPositions([0,0,0])
+    enigmaMachine.setRotorRotatePositions([5,5,5])
+
+    print(reflectorB.encrypt("A"))
+    print(reflectorB.encrypt("Y"))
+
+    #print(rotorIII.encrypt(reflectorB.encrypt(rotorIII.encrypt("A"))))
+    #print(rotorIII.encrypt(reflectorB.encrypt(rotorIII.encrypt("W"))))
